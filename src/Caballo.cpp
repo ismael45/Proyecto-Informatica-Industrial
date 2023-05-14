3eb23e73113fb681bf04e9c1b1d441806b0d351f
Irenevm <61277793+Irenevm@users.noreply.github.com>
2023-05-14 22:14:35 +0200

    Revert "Intento de merge"

--- src/Caballo.cpp
+++
@@ -1,9 +0,0 @@
-#include "Caballo.h"
-
-void Caballo::dibuja_caballo() {
-    glPushMatrix();
-    glTranslatef(posicion.x, posicion.y, 0.5);
-    glColor3f(color.r, color.g, color.b);
-    glutSolidTeapot(tamaño);
-    glPopMatrix();
-}
